0240a3eee828c5eac3c7ba570c6c4607c2f28e97
Cristina Daogaru <cristina.florentina.daogaru@gmail.com>
2019-10-19 23:05:00 +0300

    Created script for detecting click on planets and show panel with info base on the planet tag

---
+++ Atlas/Assets/OpenInfoPanel.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenInfoPanel : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void EnablePanel()
+    {
+        Panel.SetActive(true);
+    }
+
+    Ray ray;
+    RaycastHit hit;
+
+    void OnMouseDown()
+    { 
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            string getTextInfoPlanet = hit.collider.gameObject.GetComponent<Text>().ToString();
+            switch (hit.collider.tag)
+            {
+                case "Mercury":
+                    EnablePanel();
+
+                    if (Panel != null)
+                    {
+                        Animator animator = Panel.GetComponent<Animator>();
+                        Panel.GetComponent<Text>().text = getTextInfoPlanet;
+                        if (animator != null)
+                        {
+                            bool isOpen = animator.GetBool("open");
+                            animator.SetBool("open", !isOpen);
+
+                        }
+                    }
+
+                    break;
+                case "Venus":
+                    EnablePanel();
+
+                    if (Panel != null)
+                    {
+                        Animator animator = Panel.GetComponent<Animator>();
+                        Panel.GetComponent<Text>().text = getTextInfoPlanet;
+                        if (animator != null)
+                        {
+                            bool isOpen = animator.GetBool("open");
+                            animator.SetBool("open", !isOpen);
+
+                        }
+                    }
+                    break;
+                case "Earth":
+                    EnablePanel();
+
+                    if (Panel != null)
+                    {
+                        Animator animator = Panel.GetComponent<Animator>();
+                        Panel.GetComponent<Text>().text = getTextInfoPlanet;
+                        if (animator != null)
+                        {
+                            bool isOpen = animator.GetBool("open");
+                            animator.SetBool("open", !isOpen);
+
+                        }
+                    }
+                    break;
+                case "Mars":
+                    EnablePanel();
+
+                    if (Panel != null)
+                    {
+                        Animator animator = Panel.GetComponent<Animator>();
+                        Panel.GetComponent<Text>().text = getTextInfoPlanet;
+                        if (animator != null)
+                        {
+                            bool isOpen = animator.GetBool("open");
+                            animator.SetBool("open", !isOpen);
+
+                        }
+                    }
+                    break;
+                case "Jupiter":
+                    EnablePanel();
+
+                    if (Panel != null)
+                    {
+                        Animator animator = Panel.GetComponent<Animator>();
+                        Panel.GetComponent<Text>().text = getTextInfoPlanet;
+                        if (animator != null)
+                        {
+                            bool isOpen = animator.GetBool("open");
+                            animator.SetBool("open", !isOpen);
+
+                        }
+                    }
+                    break;
+                case "Saturn":
+                    EnablePanel();
+
+                    if (Panel != null)
+                    {
+                        Animator animator = Panel.GetComponent<Animator>();
+                        Panel.GetComponent<Text>().text = getTextInfoPlanet;
+                        if (animator != null)
+                        {
+                            bool isOpen = animator.GetBool("open");
+                            animator.SetBool("open", !isOpen);
+
+                        }
+                    }
+                    break;
+                case "Uranus":
+                    EnablePanel();
+
+                    if (Panel != null)
+                    {
+                        Animator animator = Panel.GetComponent<Animator>();
+                        Panel.GetComponent<Text>().text = getTextInfoPlanet;
+                        if (animator != null)
+                        {
+                            bool isOpen = animator.GetBool("open");
+                            animator.SetBool("open", !isOpen);
+
+                        }
+                    }
+                    break;
+                case "Neptune":
+                    EnablePanel();
+
+                    if (Panel != null)
+                    {
+                        Animator animator = Panel.GetComponent<Animator>();
+                        Panel.GetComponent<Text>().text = getTextInfoPlanet;
+                        if (animator != null)
+                        {
+                            bool isOpen = animator.GetBool("open");
+                            animator.SetBool("open", !isOpen);
+
+                        }
+                    }
+                    break;
+                case "Pluto":
+                    EnablePanel();
+
+                    if (Panel != null)
+                    {
+                        Animator animator = Panel.GetComponent<Animator>();
+                        Panel.GetComponent<Text>().text = getTextInfoPlanet;
+                        if (animator != null)
+                        {
+                            bool isOpen = animator.GetBool("open");
+                            animator.SetBool("open", !isOpen);
+
+                        }
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
+

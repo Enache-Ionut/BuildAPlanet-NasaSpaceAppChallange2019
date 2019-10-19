0c92718a3031f69f98d9218be2d4e8d6183f3a49
Radu Albastroiu <radu.albastroiu@caphyon.com>
2019-10-19 11:20:32 +0300

    Add rotation script

---
+++ Atlas/Assets/Scripts/Rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float xRotation;
+    public float yRotation;
+    public float zRotation;
+
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(xRotation, yRotation, zRotation);
+    }
+}

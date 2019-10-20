a206f3a4f621f1f7d73ab9ba692b66fa530d5ae5
Radu Albastroiu <albastroiuradug@gmail.com>
2019-10-20 04:36:38 +0300

    Add buttons

---
+++ Atlas/Assets/Scripts/SpinButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinButton : MonoBehaviour
+{
+    public TapToPlaceGalaxy placeObject;
+
+    private GameObject placedObject = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        placedObject = placeObject.GetPlacedObject();
+    }
+
+    public void ButtonPressed()
+    {
+        if(placedObject != null)
+        {
+            if (placeObject.isActiveAndEnabled)
+                placedObject.SetActive(false);
+            else
+                placedObject.SetActive(true);
+        }
+    }
+}

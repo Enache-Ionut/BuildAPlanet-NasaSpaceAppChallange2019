12cbe88ee759a00c661d33cfa0b2467ab4355748
Radu Albastroiu <albastroiuradug@gmail.com>
2019-10-20 07:51:44 +0300

    Raycast try

---
+++ Atlas/Assets/Scripts/FindPlanet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+using UnityEngine.XR.ARSubsystems;
+
+public class FindPlanet : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager arRaycastManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Debug.Log("Display Touch");
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                Debug.Log("Something Hit");
+                var planetInfo = raycastHit.transform.gameObject.GetComponent<PlanetInfo>();
+                if(planetInfo != null)
+                {
+                    raycastHit.transform.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
